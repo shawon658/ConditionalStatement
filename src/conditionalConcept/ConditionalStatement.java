986286873baa723c66a76dfa00102024fa3f81b3
mahmed <mahmedshawon@outlook.com>
2023-04-01 10:47:13 -0400

    first commit

---
+++ src/conditionalConcept/ConditionalStatement.java
@@ -0,0 +1,74 @@
+package conditionalConcept;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		
+		ConditionalStatement obj = new ConditionalStatement();
+		obj.ifCondition();
+		obj.elseCondition();
+		obj.ifElseIfCondition();
+		obj.nestedIf();
+		
+
+	}
+	public void ifCondition() {
+		int num = 5;
+		
+		if(num>=1) {
+			System.out.println("The number is bigger than 1");
+			
+		}
+		
+	}
+	
+	public  void elseCondition() {
+		int age = 10;
+		
+		if(age>=18) {
+			System.out.println("Allowed to enter.");
+		}
+		else {
+			System.out.println("Not allowed or Not Eligible");
+		}
+	}
+	
+	public void ifElseIfCondition() {
+		 int marks=65;  
+	      
+		    if(marks<50){  
+		        System.out.println("fail");  
+		    }  
+		    else if(marks>=50 && marks<60){  
+		        System.out.println("D grade");  
+		    }  
+		    else if(marks>=60 && marks<70){  
+		        System.out.println("C grade");  
+		    }  
+		    else if(marks>=70 && marks<80){  
+		        System.out.println("B grade");  
+		    }  
+		    else if(marks>=80 && marks<90){  
+		        System.out.println("A grade");  
+		    }else if(marks>=90 && marks<100){  
+		        System.out.println("A+ grade");  
+		    }else{  
+		        System.out.println("Invalid!");  
+		    }  
+	}
+	public void nestedIf() {
+		int age = 18;
+		String gender = "male";
+		
+		if(age>=18) {
+			if(gender.equals("male")) {
+				System.out.println("Allowed to take admission for this program.");
+			}else {
+				System.out.println("Not male.");
+			}
+		}else {
+			System.out.println("Not adult");
+		}
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mahme
+ *
+ */
+module JavaConcept {
+}
